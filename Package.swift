e4c05b2dd08fc867784670d75396cd1af719ddda
Gio Lodi <gio@mokacoding.com>
2025-11-25 11:26:43 +1100

    Track package generated by SwiftPM
    ```
    swift package init --name GenOne
    ```

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "GenOne",
+    products: [
+        .library(
+            name: "GenOne",
+            targets: ["GenOne"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "GenOne"
+        ),
+        .testTarget(
+            name: "GenOneTests",
+            dependencies: ["GenOne"]
+        ),
+    ]
+)
